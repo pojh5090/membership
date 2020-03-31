23f70aeef8c08bbb0a82dd2214cb0507cce2d7ed
pojh5090 <pojh4@naver.com>
2020-03-31 17:07:12 +0900

    chat-db

--- UserChat/src/user/UserRegisterServlet.java
+++ UserChat/src/user/UserRegisterServlet.java
@@ -1,4 +1,3 @@
-
 package user;
 
 import java.io.IOException;
@@ -23,17 +22,18 @@ public class UserRegisterServlet extends HttpServlet {
 		String userGender = request.getParameter("userGender");
 		String userEmail = request.getParameter("userEmail");
 		String userProfile = request.getParameter("userProfile");
-		if(userID == null || userID.equals("") || userPassword1 == null || userPassword1.equals("") || 
+		
+		if(userID == null || userID.equals("") || userPassword1 == null || userPassword1.equals("") ||
 				userPassword2 == null || userPassword2.equals("") || userName == null || userName.equals("") ||
-				userAge == null || userAge.equals("") || userGender == null || userGender.equals("") || userEmail == null || userEmail.equals("")) 
+				userAge == null || userAge.equals("") || userEmail == null || userEmail.equals(""))
 		{
-			request.getSession().setAttribute("messageType", "오류 메시지");
+			request.getSession().setAttribute("messageType", "오류메시지");
 			request.getSession().setAttribute("messageContent", "모든 내용을 입력하세요.");
 			response.sendRedirect("join.jsp");
 			return;
 		}
 		if(!userPassword1.equals(userPassword2)) {
-			request.getSession().setAttribute("messageType", "오류 메시지");
+			request.getSession().setAttribute("messageType", "오류메시지");
 			request.getSession().setAttribute("messageContent", "비밀번호가 서로 다릅니다.");
 			response.sendRedirect("join.jsp");
 			return;
@@ -44,10 +44,11 @@ public class UserRegisterServlet extends HttpServlet {
 			request.getSession().setAttribute("messageContent", "회원가입에 성공했습니다.");
 			response.sendRedirect("index.jsp");
 		} else {
-			request.getSession().setAttribute("messageType", "오류 메시지");
+			request.getSession().setAttribute("messageType", "오류메시지");
 			request.getSession().setAttribute("messageContent", "이미 존재하는 회원입니다.");
 			response.sendRedirect("join.jsp");
 		}
+			
 	}
 
 }
